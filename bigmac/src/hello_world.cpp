b1d7318ccd107f95d503b035a9a97ba1bfa2e91a
Samuel Farrens <samuel.farrens@gmail.com>
2020-08-04 14:40:50 +0200

    updated hello world script

--- bigmac/src/hello_world.cpp
+++ bigmac/src/hello_world.cpp
@@ -2,7 +2,11 @@
 #include <omp.h>
 
 int main(int argc, char** argv){
-    printf("Hello from process: %d\n", omp_get_thread_num());
+
+    #pragma omp parallel
+    {
+      printf("Hello from process: %d\n", omp_get_thread_num());
+    }
 
     return 0;
 }
